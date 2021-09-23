2d52152a9b0b451a99a1b950a649b0d8ec0be8aa
ezedward01 <90197592+ezedward01@users.noreply.github.com>
2021-09-23 20:04:37 +0700

    Update DanhSachNhanVien.js

--- js/model/DanhSachNhanVien.js
+++ js/model/DanhSachNhanVien.js
@@ -12,6 +12,7 @@ DanhSachNhanVien.prototype.timViTri = function(taiKhoanNV) {
         return taiKhoanNV === item.taiKhoanNV
     })
 }
+56b0091e75bb9e1d72acfa75efe569b5df08412e0b3487955ec41343e44b6d3e
 // xoa 1 phan tu ra khoi mang
 DanhSachNhanVien.prototype.xoaNV = function(taiKhoanNV) {
     
