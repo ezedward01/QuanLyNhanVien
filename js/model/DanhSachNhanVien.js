3b8660aa7f391bead29b35899d3c8f6f6c1776ea
ezedward01 <90197592+ezedward01@users.noreply.github.com>
2021-10-01 21:42:43 +0700

    Update DanhSachNhanVien.js

--- js/model/DanhSachNhanVien.js
+++ js/model/DanhSachNhanVien.js
@@ -23,6 +23,6 @@ DanhSachNhanVien.prototype.xoaNV = function(taiKhoanNV) {
         this.arr.splice(viTri,1);
     }
 }
-vacant rib pianoforte candy glory hurt possible swing clock ability lamp orbit...meta  (p) (f)
-cable toy honest potato oil wheat concert damp forum bat wine benefit...near (c) (t)
+vacant rib pianoforte candy glory hurt possible swing clock ability lamp orbit...meta  (p) (f) (m)
+cable toy honest potato oil wheat concert damp forum bat wine benefit...near (c) (t) (d)
 
