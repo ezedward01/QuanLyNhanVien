edf3b3d7a379deddcdc44a6c93aa81afc1eff090
ezedward01 <90197592+ezedward01@users.noreply.github.com>
2022-07-12 22:10:32 +0700

    Update NhanVien.js

--- js/model/NhanVien.js
+++ js/model/NhanVien.js
@@ -47,11 +47,11 @@ inch, ask, dune, despair, cell, able, sight.   (c) (s) (d)
 
 Algo_02
 until, usual, villages, catch, cancel, like, wide, world, sister, timber, loyal, fall, castle, oxygen, chalk, pole, hover, inquiry, 
-online, gospel, scene, cram, marriage, abstract, outer (v) (w) (m)
+online, gospel, scene, cram, marriage, abstract, outer. (v) (w) (m)
 
 TestnetAlgo 01
 oyster, inner, warranter, clutch, text, define, clinic, movie, repeat, virtual, then, rude, nasty, already, table, twelve, 
-second, clutch, flock, differ, eternal, offer, personal, ability, duck  (s) (p) (p)
+second, clutch, flock, differ, eternal, offer, personal, ability, duck.  (s) (p) (p)
 
 Keplr
 cosmos_01
